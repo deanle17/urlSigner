95f9f010e0e7adf009d6087b236ab8ef7da44a08
Dean Le <dinh.le@smarp.com>
2019-03-17 14:34:36 +0200

    Build package and upload to Pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='urlSigner',
+    version='0.0.1',
+    description='Library to sign a URL',
+    license='MIT',
+    packages=['urlSigner'],
+    author='deanle17',
+    author_email='lecungdinh@gmail.com',
+    keywords=['url', 'signer'],
+    url='https://github.com/deanle17/fsecure-assignment',
+    download_url='https://github.com/deanle17/fsecure-assignment/archive/0.0.1.tar.gz'
+)
