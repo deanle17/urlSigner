c0f78cbfa82fc47fd99baa8536b9f11ff9d9f485
Dean Le <dinh.le@smarp.com>
2019-03-18 22:51:30 +0200

    release 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='urlSigner',
-    version='0.0.2',
+    version='0.0.3',
     description='Library to sign a URL',
     license='MIT',
     packages=['urlSigner'],
@@ -10,5 +10,5 @@ setup(
     author_email='lecungdinh@gmail.com',
     keywords=['url', 'signer'],
     url='https://github.com/deanle17/fsecure-assignment',
-    download_url='https://github.com/deanle17/urlSigner/archive/0.0.2.tar.gz'
+    download_url='https://github.com/deanle17/urlSigner/archive/0.0.3.tar.gz'
 )
